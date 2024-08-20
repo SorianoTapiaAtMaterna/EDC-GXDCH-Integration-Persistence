944d570b4e2e331a1b1805ccce4381d2e2a514d2
Soriano Tapia, Isaac (DEV) <isaac.soriano-tapia@materna.group>
2024-08-20 10:06:12 +0200

    Gradle updates

--- launchers/connector/build.gradle.kts
+++ launchers/connector/build.gradle.kts
@@ -25,9 +25,35 @@ dependencies {
     implementation(libs.edc.data.plane.core)
     implementation(libs.edc.data.plane.http)
 
+    // Control plane SQL store extensions:
+
+    implementation(libs.edc.asset.index.sql)
+    implementation(libs.edc.contract.definition.store.sql)
+    implementation(libs.edc.contract.negotiation.store.sql)
+    implementation(libs.edc.policy.definition.store.sql)
+    implementation(libs.edc.transfer.process.store.sql)
+
    implementation(libs.edc.data.plane.kafka)
 
     implementation(project(":extensions:gxfs-catalog-extension"))
+
+    // SQL persistency dependencies:
+
+    api(libs.edc.core.spi)
+    api(libs.edc.transaction.spi)
+    api(libs.edc.contract.spi)
+    api(libs.edc.asset.spi)
+    api(libs.edc.transfer.spi)
+
+    implementation(libs.edc.transaction.datasource.spi)
+    implementation(libs.edc.transfer.spi)
+    implementation(libs.edc.sql.core)
+    implementation(libs.edc.sql.lease)
+    implementation(libs.edc.sql.pool.apache.commons)
+    implementation(libs.edc.util.lib)
+    implementation(libs.postgresql.jdbc.driver)
+    // To come with v0.8.2: implementation(libs.edc.sql.bootstrapper)
+
 }
 
 application {
